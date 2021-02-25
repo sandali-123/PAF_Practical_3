db3cbd899abe6ddba14cf7b0fdc51bc5f53fe00f
Sandali <sanwije2@gmail.com>
2021-02-26 01:05:03 +0530

    Update and Delete part finished.

--- src/com/Item.java
+++ src/com/Item.java
@@ -1,6 +1,9 @@
 package com;
 
-import java.beans.Statement;
+import java.sql.Statement;
+
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,7 +11,8 @@ import java.sql.ResultSet;
 
 
 public class Item {
-	public Connection connect()
+	private static boolean isSuccess;
+	public static Connection connect()
 	{ 
 	 Connection con = null; 
 	 
@@ -61,57 +65,131 @@ public class Item {
 	return output; 
 	}
 	
+	
+	//read
 	public String readItems()
 	{ 
-	 String output = ""; 
-	try
-	 { 
-	 Connection con = connect(); 
-	 if (con == null) 
-	 { 
-	 return "Error while connecting to the database for reading."; 
-	 } 
-	 // Prepare the html table to be displayed
-	 output = "<table border='1'><tr><th>Item Code</th>" +"<th>Item Name</th><th>Item Price</th>"
-	 + "<th>Item Description</th>" + "<th>Update</th><th>Remove</th></tr>"; 
-	 String query = "select * from items"; 
-	 
-	 Statement stmt = (Statement) con.createStatement(); 
-	 ResultSet rs = ((java.sql.Statement) stmt).executeQuery(query); 
-	 
-	 // iterate through the rows in the result set
-	 while (rs.next()) 
-	 { 
-	 String itemID = Integer.toString(rs.getInt("itemID")); 
-	 String itemCode = rs.getString("itemCode"); 
-	 String itemName = rs.getString("itemName"); 
-	 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
-	 String itemDesc = rs.getString("itemDesc"); 
-	 // Add a row into the html table
-	 output += "<tr><td>" + itemCode + "</td>"; 
-	 output += "<td>" + itemName + "</td>"; 
-	 output += "<td>" + itemPrice + "</td>";
-	 
-	 output += "<td>" + itemDesc + "</td>";
-	 // buttons
-	 output += "<td><input name='btnUpdate' " 
-	 + " type='button' value='Update'></td>"
-	 + "<td><form method='post' action='items.jsp'>"
-	 + "<input name='btnRemove' " 
-	 + " type='submit' value='Remove'>"
-	 + "<input name='itemID' type='hidden' " 
-	 + " value='" + itemID + "'>" + "</form></td></tr>"; 
-	 } 
-	 con.close(); 
-	 // Complete the html table
-	 output += "</table>"; 
-	 } 
-	catch (Exception e) 
-	 { 
-	 output = "Error while reading the items."; 
-	 System.err.println(e.getMessage()); 
-	 } 
-	return output; 
+		 String output = ""; 
+		 
+		 try
+		 { 
+			 Connection con = connect(); 
+			 
+		 if (con == null) 
+		 { 
+			 return "Error while connecting to the database for reading."; 
+		 } 
+		 
+		 
+		 // Prepare the html table to be displayed
+		 output = "<table border='1'><tr><th>Item Code</th>" +"<th>Item Name</th><th>Item Price</th>"
+		 + "<th>Item Description</th>" + "<th>Update</th><th>Remove</th></tr>"; 
+		 String query = "select * from items"; 
+		 
+		 Statement stmt = (Statement) con.createStatement(); 
+		 ResultSet rs = ((java.sql.Statement) stmt).executeQuery(query); 
+		 
+		 // iterate through the rows in the result set
+		 while (rs.next()) 
+		 { 
+		 String itemID = Integer.toString(rs.getInt("itemID")); 
+		 String itemCode = rs.getString("itemCode"); 
+		 String itemName = rs.getString("itemName"); 
+		 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
+		 String itemDesc = rs.getString("itemDesc"); 
+		 // Add a row into the html table
+		 output += "<tr><td>" + itemCode + "</td>"; 
+		 output += "<td>" + itemName + "</td>"; 
+		 output += "<td>" + itemPrice + "</td>";
+		 
+		 output += "<td>" + itemDesc + "</td>";
+		 // buttons
+		 output += "<td><input name='btnUpdate' " 
+		 + " type='button' value='Update' onclick = ></td>"
+		 + "<td><form method='post' action='items.jsp'>"
+		 + "<input name='btnRemove' " 
+		 + " type='submit' value='Remove'>"
+		 + "<input name='itemID' type='hidden' " 
+		 + " value='" + itemID + "'>" + "</form></td></tr>"; 
+		 } 
+		 con.close(); 
+		 // Complete the html table
+		 output += "</table>"; 
+		 } 
+		catch (Exception e) 
+		 { 
+			 output = "Error while reading the items."; 
+			 System.err.println(e.getMessage()); 
+		 } 
+		
+		
+		return output; 
+	}
+	
+	
+	public String UpdateItems(String itemID,String code, String name, String price, String desc)
+	{
+		
+		 String output = ""; 
+			try
+			 { 
+			 Connection con = connect(); 
+			 if (con == null) 
+			 { 
+			 return "Error while connecting to the database"; 
+			 } 
+			 // create a prepared statement
+			 String query = " Update items set itemCode='"+code+"',itemName = '"+name+"',itemPrice = '"+price+"',itemDesc = '"+desc+"'"
+					 + "where itemID ='"+itemID+"'";
+			 PreparedStatement preparedStmt = con.prepareStatement(query); 
+			/*
+			 * // binding values preparedStmt.setInt(1, 0); preparedStmt.setString(2, code);
+			 * preparedStmt.setString(3, name); preparedStmt.setDouble(4,
+			 * Double.parseDouble(price)); preparedStmt.setString(5, desc);
+			 */
+			//execute the statement
+			 preparedStmt.execute(); 
+			 con.close(); 
+			 output = "updated successfully"; 
+			 } 
+			catch (Exception e) 
+			 { 
+			 output = "Error while updating"; 
+			 System.err.println(e.getMessage()); 
+			 } 
+			return output; 
+		
+	}
+	public static boolean deleteItems(String itemID) {
+		
+		int convertedID = Integer.parseInt(itemID);
+		
+		try {
+			Connection con = connect(); 
+			 
+			 Statement stmt = (Statement) con.createStatement(); 
+		
+			
+			String sql = "delete from items where itemID = '"+convertedID+"'";
+			int r = stmt.executeUpdate(sql);
+			
+			if(r>0) {
+				
+				isSuccess = true;
+			}
+			else {
+				
+				isSuccess = false;
+			}
+			
+			
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		return isSuccess;
 	}
 
 }
